574c67a09bf15cc3f1d8752a09d7abdec75f13db
Ryan Kruse <rpkruse@ksu.edu>
2017-12-19 07:50:02 -0600

    Fixed syntax error causing build to fail

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace Mixtape
                         .AllowAnyOrigin()
                         .AllowAnyMethod()
                         .AllowAnyHeader()
-                        .AllowCredentials()
+                        .AllowCredentials();
                     });
             });
 
